7ff3559a7e3690f15865372d8452cdbd3b429e72
miraziz <mirazizmirpolatov9@gmail.com>
2024-10-31 21:45:20 +0500

    yang

--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import document_list, upload_document, generate_qr_code, upload_pdf
+from .views import document_list, upload_document, generate_qr_code
 
 urlpatterns = [
     path('', document_list, name='document_list'),
     path('upload/', upload_document, name='upload_document'),
     path('document/<int:document_id>/qr/', generate_qr_code, name='generate_qr_code'),
-    path('upload_pdf/', upload_pdf, name='upload_pdf'),
+
 ]
