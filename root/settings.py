20b81192a14b794029f7e408eae32ea54fb68f54
miraziz <mirazizmirpolatov9@gmail.com>
2024-10-31 23:28:17 +0500

    yang

--- root/settings.py
+++ root/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Adjust based on your directory structure
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- root/urls.py
+++ root/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('apps.urls')),
 
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
+              ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) + static(settings.MEDIA_URL,
                                                                                                document_root=settings.MEDIA_ROOT)
