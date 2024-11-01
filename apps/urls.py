bd3f2049aad21ef3810843987e033b03e35686b2
miraziz <mirazizmirpolatov9@gmail.com>
2024-11-01 12:49:48 +0500

    yang

--- apps/urls.py
+++ apps/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('upload/', views.upload_document, name='upload_document'),
     path('generate-qr/<int:document_id>/', views.generate_qr_code, name='generate_qr_code'),
     path('verify-pin/<int:document_id>/', views.verify_pin, name='verify_pin'),
+    path('document/<int:document_id>/delete/', views.delete_document, name='delete_document'),
 ]
 
--- apps/views.py
+++ apps/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Document
-from django.core.files.storage import FileSystemStorage
+import os
+from audioop import reverse
+
 import qrcode
 from django.http import HttpResponse
-import os
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import Document
 
 
 def document_list(request):
@@ -14,7 +16,7 @@ def upload_document(request):
     if request.method == 'POST' and request.FILES['pdf_file']:
         pdf_file = request.FILES['pdf_file']
         title = request.POST.get('title')
-        pin = request.POST.get('pin')  # Get the PIN from the form
+        pin = request.POST.get('pin')
         document = Document(title=title, pdf_file=pdf_file, pin=pin)
         document.save()
         return redirect('document_list')
@@ -27,7 +29,7 @@ def verify_pin(request, document_id):
     if request.method == 'POST':
         entered_pin = request.POST.get('pin')
         if entered_pin == document.pin:
-            return redirect(document.pdf_file.url)  # Redirect to the PDF if PIN is correct
+            return redirect(document.pdf_file.url)
         else:
             return render(request, 'verify_pin.html', {'error': 'не правильно ПИН код.', 'document_id': document_id})
 
@@ -37,30 +39,34 @@ def verify_pin(request, document_id):
 # def generate_qr_code(request, document_id):
 #     document = Document.objects.get(id=document_id)
 #
-#     # Construct the full URL
+#
 #     full_url = request.build_absolute_uri(document.pdf_file.url)
 #
-#     # Create QR code
+#
 #     qr = qrcode.make(full_url)
 #
-#     # Create an HTTP response with the QR code image
+#
 #     response = HttpResponse(content_type='image/png')
 #     qr.save(response, 'PNG')
 #
-#     # Set the Content-Disposition header to prompt download
+#
 #     response['Content-Disposition'] = 'attachment; filename="qr_code.png"'
 #
 #     return response
 
 def generate_qr_code(request, document_id):
-    # Use the URL for the PIN verification view
+
     verification_url = request.build_absolute_uri(f'/verify-pin/{document_id}/')
 
-    # Create QR code with the verification URL
     qr = qrcode.make(verification_url)
 
-    # Create an HTTP response with the QR code image
     response = HttpResponse(content_type='image/png')
     qr.save(response, 'PNG')
 
     return response
+
+def delete_document(request, document_id):
+    document = get_object_or_404(Document, id=document_id)
+    if request.method == 'POST':
+        document.delete()
+        return redirect(('document_list'))
